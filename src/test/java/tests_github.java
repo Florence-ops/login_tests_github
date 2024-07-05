61f92a2c0e7c207ae32ec4e01f23ac81e64ae6d5
flo muturi <florence.wairimu@gebeya.com>
2024-07-06 00:55:50 +0300

    first commit

--- src/test/java/tests_github.java
+++ src/test/java/tests_github.java
@@ -18,7 +18,7 @@ import java.util.Properties;
 
 import static org.junit.Assert.assertTrue;
 
-public class login_tests_github {
+public class login_tests_githubb {
     private static WebDriver driver;
     private static WebDriverWait wait;
     private static Properties properties;
